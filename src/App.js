66c90a5c63d04c0fc9f8e49752a1a60fe49b42d8
YangfanZhang <yf_chang@icloud.com>
2020-09-28 23:18:26 +1000

    Added card-list component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import CardList from "./components/card-list/card-list.component.jsx"
 
 class App extends Component {
   constructor() {
@@ -31,6 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <CardList/>
         {this.state.monsters.map((monster) => (
           <h1>{monster.name}</h1>
         ))}
