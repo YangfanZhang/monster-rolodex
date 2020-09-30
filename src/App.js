0178a9a2bc79e209fdbf0050b9fc1cb07c172b36
YangfanZhang <yf_chang@icloud.com>
2020-09-30 21:53:33 +1000

    Created handleChange method.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class App extends Component {
     fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => this.setState({monsters: users}))
   }
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value});
+  }
+
   render() {
     const {monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
@@ -25,7 +29,7 @@ class App extends Component {
 
     return (
       <div className="App">
-      <SearchBox placeholder='search monsters' handleChange={e=> this.setState({searchField: e.target.value})}/>
+      <SearchBox placeholder='search monsters' handleChange={this.handleChange}/>
       <CardList monsters={filteredMonsters}/>
       </div>
     );
